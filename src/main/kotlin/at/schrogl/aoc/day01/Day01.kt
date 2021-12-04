03b250fb3c943df8712b55f4fe15dbd45caece06
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-12-04 18:52:26 +0100

    Day 1 Part 1
    Initial implementation. Computation with test data works, but real
    data produces 1461 as result, which is too low

---
+++ src/main/kotlin/at/schrogl/aoc/day01/Day01.kt
@@ -0,0 +1,42 @@
+/*-
+ * #%L
+ * Advent-Of-Code 2021
+ * %%
+ * Copyright (C) 2021 Fritz Schrogl
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package at.schrogl.aoc.day01
+
+class Day01 {
+
+    fun solution1(filename: String): Int {
+        val input = Day01::class.java.getResource(filename)
+            ?.readText(Charsets.UTF_8)
+            ?.split("\n")
+            ?: throw NullPointerException("Input file not found")
+
+        var counter = 0
+        for (i in 1 until input.size) counter += if (input[i - 1] < input[i]) 1 else 0
+        return counter
+    }
+
+}
+
+fun main() {
+    println("day01-solution1-test: ${Day01().solution1("day01a.test.txt")}")
+    println("day01-solution1: ${Day01().solution1("day01a.input.txt")}")
+}
