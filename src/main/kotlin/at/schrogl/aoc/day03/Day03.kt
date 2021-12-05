f8c5a2d9fd9364eaae974e2718411cf0d5fa5fc4
Fritz Schrogl <fschrogl@users.noreply.github.com>
2021-12-06 00:50:21 +0100

    Day 3 Part 2 done

--- src/main/kotlin/at/schrogl/aoc/day03/Day03.kt
+++ src/main/kotlin/at/schrogl/aoc/day03/Day03.kt
@@ -37,12 +37,39 @@ fun List<String>.solution1(): Int {
     return gamma.toInt(2) * epsilon.toInt(2)
 }
 
+fun List<String>.solution2(): Int {
+    var lifeSupportRating = ""
+    for (i in 0 until this[0].length) lifeSupportRating += getMostCommonChar(lifeSupportRating, i)
+
+
+    var co2ScrubberRating = ""
+    for (i in 0 until this[0].length) co2ScrubberRating += getLeastCommonChar(co2ScrubberRating, i)
+
+    return lifeSupportRating.toInt(2) * co2ScrubberRating.toInt(2)
+}
+
+fun List<String>.getMostCommonChar(prefix: String, pos: Int): String {
+    val filteredList = filter { prefix.isEmpty() || it.startsWith(prefix) }
+    val ones = filteredList.count { it[pos] == '1' }
+    return if (filteredList.size == 1) filteredList[0][pos].toString() else if (ones >= filteredList.size - ones) "1" else "0"
+}
+
+fun List<String>.getLeastCommonChar(prefix: String, pos: Int): String {
+    val filteredList = filter { prefix.isEmpty() || it.startsWith(prefix) }
+    val ones = filteredList.count { it[pos] == '1' }
+    return if (filteredList.size == 1) filteredList[0][pos].toString() else if (ones < filteredList.size - ones) "1" else "0"
+}
+
 fun main() {
     println(
         """
         Day 3-1
             test1 = ${Day03().readInput("input-test.txt").solution1()} (expected: 198)
             ex1   = ${Day03().readInput("input.txt").solution1()}
+            
+        Day 3-2
+            test1 = ${Day03().readInput("input-test.txt").solution2()} (expected: 230)
+            ex2   = ${Day03().readInput("input.txt").solution2()}
     """.trimIndent()
     )
 }
